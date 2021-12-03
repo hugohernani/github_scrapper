20a5e2c6c0610e7aa9a41db643ea88e58f0584b3
Hugo Hernani <hhernanni@gmail.com>
2021-12-03 18:05:17 -0300

    Add bootstrap into webpack settings and adjust the later for hot reloading changes on views as well

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,11 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import 'bootstrap'
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import "../stylesheets/application"
 import "channels"
 
 Rails.start()
