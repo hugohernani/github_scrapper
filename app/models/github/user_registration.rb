87082bccaa67f41f4084c2c5d993c6855b4afb5d
Hugo Hernani <hhernanni@gmail.com>
2021-12-03 00:14:17 -0300

    Include UserRegistration use case to orchestrate collaborators work to create user, shorten link and scrap github profile data

--- app/models/github/user_registration.rb
+++ app/models/github/user_registration.rb
@@ -1,20 +1,52 @@
 module Github
   class UserRegistration
-    def initialize(github_user_form, link_shorten:)
+    def initialize(github_user_form, link_shorten:, web_scrapper: WebScrapper::GithubProfile.new)
       @github_user_form = github_user_form
       @link_shorten     = link_shorten
+      @web_scrapper     = web_scrapper
     end
 
     def create
-      User.create(
-        name: github_user_form.name,
-        url: github_user_form.url
-      )
-      link_shorten.generate(url: github_user_form.url)
+      user         = persist_user
+      short_url    = shorten_url
+      profile_data = webscrap_github_profile_data
+      persist_short_url_on_user(user.id, short_url)
+      persist_github_profile(user.id, profile_data)
     end
 
     private
 
-    attr_reader :github_user_form, :link_shorten
+    attr_reader :github_user_form, :link_shorten, :web_scrapper
+
+    def persist_user
+      User.create(
+        name: github_user_form.name,
+        url: github_user_form.url
+      )
+    end
+
+    def shorten_url
+      link_shorten.generate(url: github_user_form.url)
+    end
+
+    def webscrap_github_profile_data
+      web_scrapper.load_data(url: github_user_form.url)
+    end
+
+    def persist_short_url_on_user(user_id, short_url)
+      User.add_short_url(user_id: user_id, short_url: short_url)
+    end
+
+    def persist_github_profile(user_id, profile)
+      GithubProfile.create(
+        user_id: user_id,
+        username: profile.username,
+        followers: profile.followers,
+        following: profile.following,
+        stars: profile.stars,
+        contributions: profile.contributions,
+        image_url: profile.image_url
+      )
+    end
   end
 end
--- spec/models/github/user_registration_spec.rb
+++ spec/models/github/user_registration_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
-describe Github::UserRegistration do
+describe Github::UserRegistration, github_fake_response: true do
   subject(:registration){ described_class.new(user_form, link_shorten: mocked_shorten_link) }
 
-  let(:mocked_shorten_link){ double(:mocked_shorten_link, generate: nil) }
+  let(:fake_short_url){ Faker::Internet.url(host: 'bit.ly') }
+  let(:mocked_shorten_link){ double(:mocked_shorten_link, generate: fake_short_url) }
 
   let(:user_form) do
     GithubUserRegistrationForm.new(
@@ -11,19 +12,51 @@ describe Github::UserRegistration do
     )
   end
 
-  describe 'creation of an user from given form' do
+  describe 'user record registration' do
     it 'registers it into database' do
       expect do
         registration.create
       end.to change(User, :count)
     end
+
+    it 'updates short_url from returned fake short url on created user' do
+      registration.create
+
+      user = User.last
+      expect(user.short_url).to eq(fake_short_url)
+    end
   end
 
-  describe 'generation of a short link' do
+  describe 'using external services' do
     it 'delegates shortening to given shorten service ' do
       registration.create
 
       expect(mocked_shorten_link).to have_received(:generate).with(url: user_form.url)
+    end
+
+    context 'when scraping profile data' do
+      subject(:registration) do
+        described_class.new(user_form, link_shorten: mocked_shorten_link, web_scrapper: fake_web_scrapper)
+      end
+
+      let(:profile_data) do
+        OpenStruct.new(attributes_for(:github_profile))
+      end
+      let(:fake_web_scrapper){ instance_double('WebScrapper::GithubProfile', load_data: profile_data) }
+
+      it 'creates a GithubProfile database record with incoming data from webscrapper' do
+        registration.create
+
+        created_github_profile = GithubProfile.last
+        expect(created_github_profile).to have_attributes({
+                                                            username: profile_data.username,
+                                                            followers: profile_data.followers,
+                                                            following: profile_data.following,
+                                                            stars: profile_data.stars,
+                                                            contributions: profile_data.contributions,
+                                                            image_url: profile_data.image_url
+                                                          })
+      end
     end
   end
 end
