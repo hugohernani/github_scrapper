e8668eb53a5047d776c84d43a03c6b08f1372104
Hugo Hernani <hhernanni@gmail.com>
2021-12-04 01:14:26 -0300

    Add github profile method into User to create/update github_profile with given data

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ApplicationRecord
   def self.add_short_url(user_id:, short_url:)
     where(id: user_id).update_all(short_url: short_url)
   end
+
+  def self.persist_github_profile(user_id:, profile:)
+    github_profile = GithubProfile.find_or_create_by(user_id: user_id)
+    github_profile.update(
+      username: profile.username,
+      followers: profile.followers,
+      following: profile.following,
+      stars: profile.stars,
+      contributions: profile.contributions,
+      image_url: profile.image_url
+    )
+  end
 end
